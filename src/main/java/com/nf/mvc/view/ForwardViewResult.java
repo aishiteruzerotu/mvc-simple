ce5a76157122a6d79018016587f5e1b77db72538
杨天晨 <3138864192@qq.com>
2023-04-24 15:15:30 +0800

    为请求转发增加数据

--- src/main/java/com/nf/mvc/view/ForwardViewResult.java
+++ src/main/java/com/nf/mvc/view/ForwardViewResult.java
@@ -6,16 +6,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ForwardViewResult extends ViewResult {
     private String url;
+    private Map<String, Object> model;
 
     public ForwardViewResult(String url) {
+        this(url, new HashMap<>());
+    }
+
+    public ForwardViewResult(String url, Map<String, Object> model) {
         this.url = url;
+        this.model = model;
+
     }
 
     @Override
     public void render(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher(url).forward(req,resp);
+        req.getRequestDispatcher(url).forward(req, resp);
+    }
+
+    private void initModel(HttpServletRequest req) {
+        for (Map.Entry<String, Object> entry : model.entrySet()) {
+            req.setAttribute(entry.getKey(), entry.getValue());
+        }
     }
 }
--- src/main/java/com/nf/mvc/view/RedirectViewResult.java
+++ src/main/java/com/nf/mvc/view/RedirectViewResult.java
@@ -6,17 +6,46 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RedirectViewResult extends ViewResult {
 
     private String url;
+    private Map<String, Object> model;
 
     public RedirectViewResult(String url) {
+        this(url, new HashMap<>());
+    }
+
+    public RedirectViewResult(String url, Map<String, Object> model) {
         this.url = url;
+        this.model = model;
     }
 
     @Override
     public void render(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.url += initModel();
         resp.sendRedirect(url);
     }
+
+    private String initModel() {
+        if (model.size() == 0) return "";
+
+        StringBuilder builder = new StringBuilder("?");
+        for (Map.Entry<String, Object> entry : model.entrySet()) {
+            builder.append(entry.getKey());
+            builder.append("=");
+            builder.append(entry.getValue());
+            builder.append("&");
+        }
+
+        //去 & 连接符
+        if (builder.length() >= 2) {
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        return builder.toString();
+    }
+
+
 }
