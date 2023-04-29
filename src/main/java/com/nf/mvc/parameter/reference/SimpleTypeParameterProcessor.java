87f92187c0071c62f6f9f90002bc543ac3596d22
杨天晨 <3138864192@qq.com>
2023-04-29 17:44:10 +0800

    普通类型处理

---
+++ src/main/java/com/nf/mvc/parameter/reference/SimpleTypeParameterProcessor.java
@@ -0,0 +1,95 @@
+package com.nf.mvc.parameter.reference;
+
+import com.nf.Convert;
+import com.nf.Reflection;
+import com.nf.mvc.Handler;
+import com.nf.mvc.ParameterProcessor;
+import com.nf.mvc.annotation.RequestParam;
+import com.nf.mvc.annotation.ValueConstants;
+import com.nf.mvc.exception.exceptions.NoAssignmentToPrimitiveIsNullException;
+import com.nf.mvc.exception.exceptions.UnableToProcessTypeException;
+import com.nf.mvc.support.Order;
+import com.nf.mvc.util.ReflectionUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+import java.util.Set;
+
+@Order(0)
+public class SimpleTypeParameterProcessor implements ParameterProcessor {
+
+    protected Parameter parameter;
+
+    protected Class<?> parameterType;
+
+    @Override
+    public boolean supports(Parameter parameter) {
+        this.parameter = parameter;
+        this.parameterType = parameter.getType();
+        if (Reflection.isSimpleProperty(this.parameterType)) {
+            return true;
+        }
+        if (Reflection.isAssignableToAny(this.parameterType, Set.class, List.class)) {
+            // 只支持简单类型 List Set 集合
+            ParameterizedType parameterizedType = (ParameterizedType) this.parameter.getParameterizedType();
+            Class<?> genericType = (Class<?>) parameterizedType.getActualTypeArguments()[0];
+            return Reflection.isSimpleType(genericType);
+        }
+        return false;
+    }
+
+    @Override
+    public Object processor(Handler handler, HttpServletRequest req) throws IOException, ServletException {
+        Object value;
+        String paramName = getKey(handler);
+        //获取参数类型
+        Class<?> paramType = this.parameterType;
+        //转换为数组、集合、或具体类型的值
+        try {
+            if (paramType.isArray()) {
+                Class<?> componentType = paramType.getComponentType();
+                value = Convert.toSimpleTypeArray(componentType, req.getParameterValues(paramName));
+            } else if (List.class.isAssignableFrom(paramType) || Set.class.isAssignableFrom(paramType)) {
+                //获取集合泛型参数类型
+                ParameterizedType type = (ParameterizedType) this.parameter.getParameterizedType();
+                Class<?> genericType = (Class) type.getActualTypeArguments()[0];
+                value = Convert.toSimpleCollection(paramType, genericType, req.getParameterValues(paramName));
+            } else {
+                //比如String name,简单类型解析要考虑RequestParam注解
+                //如果出现转换异常？比如前端没有传递值，那么转成Integer这种类型就报异常
+                //前端没有传值，那么getP就是null，赋给String这种是可以，但这个defaultValue要开始生效
+
+                //toSimpleTypeValue方法的实现，是转换出了异常就返回null
+                value = Convert.toSimpleTypeValue(paramType, req.getParameter(paramName));
+                if (value == null && Reflection.isPrimitive(paramType)) {
+                    throw new NoAssignmentToPrimitiveIsNullException("不能把null给简单类型");
+                }
+
+                if (value == null && this.parameter.isAnnotationPresent(RequestParam.class)) {
+                    String defaultValue = this.parameter.getDeclaredAnnotation(RequestParam.class).defaultValue();
+                    if (!defaultValue.equals(ValueConstants.DEFAULT_NONE)) {
+                        value = defaultValue;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            return new UnableToProcessTypeException("无法转换值异常");
+        }
+        return value;
+    }
+
+    protected String getKey(Handler handler) {
+        String key = handler.getParamName(this.parameter);
+        if (this.parameter.isAnnotationPresent(RequestParam.class)) {
+            String value = this.parameter.getAnnotation(RequestParam.class).value();
+            if (!value.equals(ValueConstants.DEFAULT_NONE)) {
+                key = value;
+            }
+        }
+        return key;
+    }
+}
